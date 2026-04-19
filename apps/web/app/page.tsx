9fe8787759469c287f1d09962a74ed7e9c2545e3
Daddyjohn63 <john@webandprosper.co.uk>
2026-04-19 16:14:23 +0100

    first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,19 @@
 import { Button } from "@workspace/ui/components/button"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
-          <h1 className="font-medium">Project ready!</h1>
+          <h1 className="font-medium">Apps Web Project ready!</h1>
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
           <Button className="mt-2">Button</Button>
         </div>
-        <div className="text-muted-foreground font-mono text-xs">
+        <div className="font-mono text-xs text-muted-foreground">
           (Press <kbd>d</kbd> to toggle dark mode)
         </div>
       </div>
