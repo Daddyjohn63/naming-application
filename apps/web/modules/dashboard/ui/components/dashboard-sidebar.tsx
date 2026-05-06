7c2c136cb8671399a70bffc1df906a950e5f4541
Daddyjohn63 <john@webandprosper.co.uk>
2026-05-06 13:12:51 +0100

    feat: enhance sidebar component with improved responsiveness and accessibility features, update layout for better user experience

---
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { UserButton } from "@clerk/nextjs"
+import { Cat, HomeIcon, UsersIcon, SettingsIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export const DashboardSidebar = () => {
+  return <div>DashboardSidebar</div>
+}
